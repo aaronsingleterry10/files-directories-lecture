308ecf79add3e31ac125eef066fe26a1fb763aa5
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-18 21:14:27 -0500

    completed file and directory lecture

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace FileDirectory
 {
@@ -10,6 +11,30 @@ namespace FileDirectory
     {
         static void Main(string[] args)
         {
+            //var path = @"c:\somefile.jpg";
+            //File.Copy(@"c:\temp\myfile.jpg", @"d:\temp\myfile.jpg", true);
+            //File.Delete(path);
+            //if (File.Exists(path))
+            //{
+            //    //
+            //}
+            //var content = File.ReadAllText(path);
+
+            //var fileInfo = new FileInfo(path);
+
+            //Directory.CreateDirectory(@"c:\temp\folder1");
+            //var files = Directory.GetFiles(@"C:\Users\Aaron Singleterry\source\repos", "*.sln", SearchOption.AllDirectories);
+            //foreach (var file in files)
+            //{
+            //    Console.WriteLine(file);
+            //}
+
+            var directories = Directory.GetDirectories(@"C:\Users\Aaron Singleterry\source\repos", "*.*", SearchOption.AllDirectories);
+            foreach (var directory in directories)
+            {
+                Console.WriteLine(directory);
+            }
+
         }
     }
 }
