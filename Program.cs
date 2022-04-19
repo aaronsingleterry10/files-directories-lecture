058bb6696e5ef61bdeffc697a7b676ec7068f80e
Aaron Singleterry <aaronsingleterry10@gmail.com>
2022-04-18 21:22:52 -0500

    completed paths lecture

--- Program.cs
+++ Program.cs
@@ -29,11 +29,29 @@ namespace FileDirectory
             //    Console.WriteLine(file);
             //}
 
-            var directories = Directory.GetDirectories(@"C:\Users\Aaron Singleterry\source\repos", "*.*", SearchOption.AllDirectories);
-            foreach (var directory in directories)
-            {
-                Console.WriteLine(directory);
-            }
+            //var directories = Directory.GetDirectories(@"C:\Users\Aaron Singleterry\source\repos", "*.*", SearchOption.AllDirectories);
+            //foreach (var directory in directories)
+            //{
+            //    Console.WriteLine(directory);
+            //}
+
+            //Directory.Exists("..");
+
+            //var directoryInfo = new DirectoryInfo("...");
+            //directoryInfo.GetFiles();
+
+            var path = @"C:\Users\Aaron Singleterry\source\repos\FileDirectory\FileDirectory.sln";
+
+            // don't wanna do this
+            var dotIndex = path.IndexOf('.');
+            var extention = path.Substring(dotIndex);
+
+            // do this instead
+
+            Console.WriteLine("Ext.: " + Path.GetExtension(path));
+            Console.WriteLine("Filename: " + Path.GetFileName(path));
+            Console.WriteLine("Filename without ext: " + Path.GetFileNameWithoutExtension(path));
+            Console.WriteLine("Directory name: " + Path.GetDirectoryName(path));
 
         }
     }
